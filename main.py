5f9dfeaa2132a645685c2a78930834ec61170648
jortle <johnpmanderson@pm.me>
2025-01-01 23:40:29 -0500

    add 60fps and circleshape.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 0, 0))
-
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
