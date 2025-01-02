5809ce6d305767a6e56d8448c26ee000b7364ca7
jortle <johnpmanderson@pm.me>
2025-01-02 00:09:01 -0500

    add player triangle

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 from constants import *
-
+from player import Player
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +15,9 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
