f5638e0b707b10e9f622a372ea1e1338d767cb15
jortle <johnpmanderson@pm.me>
2025-01-02 10:23:48 -0500

    add movement and rotation, also started asteroid class

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
 from constants import *
 from player import Player
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+
 def main():
     pygame.init()
 
@@ -13,18 +19,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill("black")
 
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 if __name__ == "__main__":
     main()
